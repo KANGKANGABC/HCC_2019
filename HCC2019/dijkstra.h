fab2aafacbed83ba10f63aad16cc41d384e3667b
yangfei <yangfei4546@gmail.com>
2019-03-11 10:51:19 +0800

    增加dijkstra算法求最短路径并打印最短路径信息，目前需要手动输入点和边的信息构造邻接矩阵

---
+++ HCC2019/dijkstra.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+struct Dis {
+	string path;
+	int value;
+	bool visit;
+	Dis() {
+		visit = false;	//判断是否已经被访问
+		value = 0;		//路径的长度
+		path = "";		//路径的经过
+	}
+};
+
+class Graph_DG {
+private:
+	int vexnum;		//图的顶点个数
+	int edge;		//图的边数
+	int **arc;		//邻接矩阵
+	Dis *dis;		//记录各个顶点的最短路径信息
+	
+public:
+	//构造函数
+	Graph_DG(int vexnum, int edge);
+	//析构函数
+	~Graph_DG();
+	// 判断我们每次输入的的边的信息是否合法
+	//顶点从1开始编号
+	bool check_edge_value(int start, int end, int weight);
+	//创建图
+	void createGraph();
+	//打印邻接矩阵
+	void print();
+	//利用dijkstra算法求最短路径，输入起点
+	void Dijkstra(int begin);
+	//打印最短路径
+	void print_path(int);
+};
