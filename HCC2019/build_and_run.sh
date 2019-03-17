c0b025d9ce19bd24d0b8a8c003a341612af10111
KangZhao <kkzhaokang@gmail.com>
2019-03-17 11:23:06 +0800

    增加vs工程到linux下的迁移脚本，linux下可以编译运行

---
+++ HCC2019/build_and_run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sh build.sh
+cd bin
+./CodeCraft-2019 ../config_test/car.txt ../config_test/road.txt ../config_test/cross.txt ../config_test/answer.txt
---
+++ HCC2019/vs2linux.sh
@@ -0,0 +1,7 @@
+if [ -d "./CodeCraft-2019" ]; then
+	rm -r CodeCraft-2019
+fi
+mkdir CodeCraft-2019
+cp CMakeLists.txt ./CodeCraft-2019
+cp *.cpp ./CodeCraft-2019
+cp *.h ./CodeCraft-2019
