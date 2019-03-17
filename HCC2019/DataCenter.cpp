1f3f991995240fc6053e0fbe3ec03842f47f2601
KangZhao <kkzhaokang@gmail.com>
2019-03-17 17:02:49 +0800

    修改算法A参数

--- HCC2019/DataCenter.cpp
+++ HCC2019/DataCenter.cpp
@@ -144,7 +144,7 @@ void DataCenter::readCarData()
 		car[i - 1].idCrossTo = carTask[i - 1][2];
 		car[i - 1].speed = carTask[i - 1][3];
 		car[i - 1].plantime = carTask[i - 1][4];
-		car[i - 1].starttime = carTask[i - 1][4] + i%400;//这里给自己挖了一个坑
+		car[i - 1].starttime = carTask[i - 1][4] + i%1000;//这里给自己挖了一个坑
 		car[i - 1].status = SLEEPING;//车的初始状态为SLEEPING
 		car[i - 1].dirCross = NONE;//车的过路口状态为NONE
 
