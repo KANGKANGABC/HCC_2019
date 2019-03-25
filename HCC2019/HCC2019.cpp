336d06593c0f90d0224a81f846e86c1378591d24
Kang Zhao <kkzhaokang@gmail.com>
2019-03-26 00:29:14 +0800

    调度器精度测试

--- HCC2019/HCC2019.cpp
+++ HCC2019/HCC2019.cpp
@@ -62,8 +62,8 @@ int main(int argc, char *argv[])
 	//dc.writeResult(answer_file);
 	int time = 0;
 	int para = 0;
-	//para = sd.getParaByScheduler();
-	time = sd.getPathByScheduler(5);
+	para = sd.getParaByScheduler();
+	//time = sd.getPathByScheduler(5);
 	//sd.getPathByTime();//获得车辆的路径信息
 	//sd.reorderCars();//按照时间重排序车辆
 	//sd.getStartTime_loadbalance(550);
